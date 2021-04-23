4172eb8518c1403b0458cdecf951db7cf79376ff
Vincent <sk8_vince@hotmail.fr>
2021-04-23 11:06:02 +0200

    - MVP done. WIP : Work section

--- js/page_transition/contactTransition.js
+++ js/page_transition/contactTransition.js
@@ -1,13 +1,13 @@
 import Highway from '@dogstudio/highway';
-import {gsap, TimelineLite} from 'gsap';
+import {gsap} from 'gsap';
 import {CustomEase} from 'gsap/CustomEase';
 
 gsap.registerPlugin(CustomEase);
+const customEase = CustomEase.create("custom", ".75,.01,.25,.99");
 
 class ContactTransition extends Highway.Transition {
     in({from, to, done}) {
-        const tl = new TimelineLite();
-        tl.fromTo(to,
+        gsap.fromTo(to,
             {left: '100%'},
             {
                 left: '0%',
@@ -18,10 +18,33 @@ class ContactTransition extends Highway.Transition {
                     done();
                 }
             });
+        animElementTransition(to, true)
     }
 
     out({from, done}) {
+        animElementTransition(from, false)
         done();
+    }
+}
+
+/**
+ *
+ * @param {HTMLElement} container
+ * @param {boolean} inOutTransition - TRUE for in transition, FALSE for out transition
+ */
+function animElementTransition(container, inOutTransition ) {
+    const title = container?.querySelector('.contact-title');
+    const description = container?.querySelector('.contact-description');
+    const linkContainer = container?.querySelector('.contact-link');
+
+    if (inOutTransition){
+        gsap.fromTo(title, {translateX: '25px', opacity: 0}, {translateX: '0', opacity: 1, duration: 1, delay:.7});
+        gsap.fromTo(description, {translateX: '25px', opacity: 0}, {translateX: '0', opacity: 1, duration: 1, delay:.8 });
+        gsap.fromTo(linkContainer, {translateX: '25px', opacity: 0}, {translateX: '0', opacity: 1, duration: 1, delay:.8 });
+    } else {
+        gsap.fromTo(title, {translateX: '0'}, {translateX: '70.72135785007072vw', duration: 1.5,  ease: customEase});
+        gsap.fromTo(description, {translateX: '0'}, {translateX: '70.72135785007072vw', duration: 1.5, ease: customEase });
+        gsap.fromTo(linkContainer, {translateX: '0'}, {translateX: '70.72135785007072vw', duration: 1.5, ease: customEase });
     }
 }
 
